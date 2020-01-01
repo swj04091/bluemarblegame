59503ab8a9cbec056e5b2047e1e4673a3fce05f3
SonDongHyun <blue951208@naver.com>
2020-01-01 14:06:08 +0900

    <손동현> 주사위 월급 이벤트 추가
    play.js : 2번째 턴 오류 수정 및 한바퀴를 돌았을때 월급 20만원 추가 이벤트 생성

--- bluemarblegame/WebContent/scripts/play.js
+++ bluemarblegame/WebContent/scripts/play.js
@@ -8,43 +8,73 @@ $(document).ready(function(){
 	let secondDice = 0;
 	
 	//위치 변수
-	let position = 1;
+	let position;
 	let move=0;
 	let turn=0;
-	
+	let play=1;
+	let gamer;
 	
 	function movedice(){
+		console.log(turn);
+		gamer=player[turn];
+		console.log(gamer);
 		firstDice = Math.floor(Math.random()*6+1);//랜덤 출력 1~6
 		secondDice = Math.floor(Math.random()*6+1);//랜덤 출력 1~6
 		console.log("f:",firstDice,"s:",secondDice);
-		player[turn].afterPoint = player[turn].beforePoint + firstDice + secondDice;
+		
+		if(gamer.beforePoint==31 || gamer.beforePoint==11){//무인도랑 우주여행일때 이동 X
+			move=0;
+		}
+		
+		//이전 위치
+		gamer.beforeId = "#p"+gamer.beforePoint; 
+		//주사위 돌리기 전 이미지 삭제
+		//$(gamer.beforeId).find(".player"+gamer.player).remove();
+
+		move=firstDice+secondDice;
+		gamer.afterPoint = gamer.beforePoint + move;		
+		console.log("확인>",gamer);
+		//주사위 돌린 후 위치 태그
+		afterId = "#p"+gamer.afterPoint;
+		
+		//캐릭터 출력
+		//$(gamer.afterId).append("<input id='pl' class='player"+gamer.player+"' type= 'image' src='/bluemarblegame/img/pin"+gamer.player+".png'>");
+		
 		//한바퀴 돌았을때
-		if(player[turn].afterPoint>40){
-			player[turn].afterPoint = player[turn].afterPoint - 40;
-			player[turn].round+=1;
+		if(gamer.afterPoint>40){
+			gamer.afterPoint =gamer.afterPoint - 40;
+			gamer.round+=1;
+			gamer.money = gamer.money + 200000;	//---------------월급			
+			console.log("월급 추가money : ",gamer.money);
 		}
 		console.log(turn,"번째");
-		console.log("위치:",player[turn].afterPoint);
+		console.log("위치:",gamer.afterPoint);
 		//더블 이벤트
 		if(firstDice==secondDice){
 			console.log("double");
-			player[turn].beforePoint = player[turn].afterPoint
-			console.log("위치wA:",player[turn].beforePoint);
+		
+			gamer.beforePoint = gamer.afterPoint
+			console.log("위치wA:",gamer.beforePoint);
+			position="#p"+gamer.beforePoint;
+			
 			console.log(turn,"번째");
 		}else{
-			console.log("위치A:",player[turn].afterPoint);
-			player[turn].beforePoint = player[turn].afterPoint
-			console.log("위치wA:",player[turn].beforePoint);
+			console.log("위치A:",gamer.afterPoint);
+			gamer.beforePoint = gamer.afterPoint
+			console.log("위치wA:",gamer.beforePoint);
 			//차례를 넘긴다.
 			turn+=1;
 			if(turn>3){
 				turn=0;
 			}
 		}
-		return player[turn].beforePoint
+	
+		return gamer.beforePoint
 	}
-	//console.log(player[turn]);
-	$("#dice").click(function(){movedice()});
+	
+	$("#dice").click(function(){
+		movedice()
+		});
 
 	
 });

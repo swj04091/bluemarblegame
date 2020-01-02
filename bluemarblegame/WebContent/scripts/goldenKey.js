e2dd8329f9ee71ab9df235f8e3204a803e920e20
swj04091 <swj04091@naver.com>
2020-01-02 14:06:45 +0900

    <권혁태> 황금열쇠 버그 수정
    -> player -> gamer.player로 수정

--- bluemarblegame/WebContent/scripts/goldenKey.js
+++ bluemarblegame/WebContent/scripts/goldenKey.js
@@ -22,18 +22,18 @@ let goldenKey = function(){
  	  console.log("황금열쇠 "+cardno);
  	  console.log(goldenCard);
  	   
- 	   // 카드 내용 보여주기
- 	   setTimeout(function() {
- 	   alert(goldenCard);
- 	   }, 0)
- 	   
- 	   // 카드 내용 이행
- 	   switch(cardno){
- 	   	case 1:	//병원비 지불
- 	   		minusMoney(gamer,50000);
+ 	  // 카드 내용 보여주기
+ 	  setTimeout(function() {
+ 	  alert(goldenCard);
+ 	  }, 0)
+ 	  
+ 	  // 카드 내용 이행
+ 	  switch(cardno){
+ 	  	case 1:	//병원비 지불
+ 	  		minusMoney(gamer.player,50000);
  	   		break;
  	   	case 2:	//복권당첨
- 	   		plusMoney(gamer,200000);
+ 	   		plusMoney(gamer.player,200000);
  	   		break;
  	   	case 3:	//꽝
  	   		break;
@@ -41,83 +41,83 @@ let goldenKey = function(){
 			case 4: //꽝
  	   		break;
  	   	case 5: //무인도
- 	   		cardMove(gamer,11,11);
+ 	   		cardMove(gamer.player,11,11);
  	   		break;
  	   	case 6: //관광여행(제주)
- 	   		cardMove(gamer,6,6);
+ 	   		cardMove(gamer.player,6,6);
  	   		break;
  	   	case 7: //과속운전벌금
- 	   		minusMoney(gamer,50000);
+ 	   		minusMoney(gamer.player,50000);
  	   		break;
  	   	case 8: //해외유학
- 	   		minusMoney(gamer,100000);
+ 	   		minusMoney(gamer.player,100000);
  	   		break;
  	   	case 9: //연금 혜택
- 	   		plusMoney(gamer,50000);
+ 	   		plusMoney(gamer.player,50000);
  	   		break;
  	   	case 10: //이사(3칸 뒤로)
- 	   		cardMove(gamer,gamer.beforePoint - 3,gamer.beforePoint - 3);
+ 	   		cardMove(gamer.player,gamer.beforePoint - 3,gamer.beforePoint - 3);
  	   		break;
  	   	case 11: // 고속도로
- 	   		cardMove(gamer,1,1);
+ 	   		cardMove(gamer.player,1,1);
  	   		break;
  	         /*
  	        	 만약에 한바퀴 도는거면 + 20만원
  	         */
  	   	case 12: //우승
- 	   		plusMoney(gamer,100000);
+ 	   		plusMoney(gamer.player,100000);
  	   		break;
  	   	case 13: //꽝
  	   		
  	   		break;
  	   	case 14: //항공여행(콩코드 -> 타이페이)
- 	   		cardMove(gamer,2,2);
+ 	   		cardMove(gamer.player,2,2);
  	   		break;
  	   		/*
  	   		만약에 콩코드 여객기를 누군가 가지고 있으면 결제, 타이페이도 마찬가지로 결제
  	   		*/
  	   	case 15: //유람선여행(퀸엘리자베스-> 베이징)
- 	   		cardMove(gamer,4,4)
+ 	   		cardMove(gamer.player,4,4)
  	   		break;
  	   		/*
  	   		퀸엘리자베스호 결제, 베이징 결제
  	   		*/
  	   	case 16: //관광여행(부산)
- 	   		cardMove(gamer,26,26)
+ 	   		cardMove(gamer.player,26,26)
  	   		break;
  	   		/*
  	   		부산 결제
  	   		*/
  	   	case 17: // 생일 축하
  	   		let birth =  10000 * (allPlayer-1);
- 	   		minusMoney(gamer,birth);
+ 	   		minusMoney(gamer.player,birth);
  	   		break;
  	   	case 18: //장학금 혜택
- 	   		plusMoney(gamer,100000);
+ 	   		plusMoney(gamer.player,100000);
  	   		break;
  	   	case 19: //노벨평화상
- 	   		plusMoney(gamer,300000);
+ 	   		plusMoney(gamer.player,300000);
  	   		break;
  	   	case 20: //관광여행(서울)
- 	   		cardMove(gamer,40,40);
+ 	   		cardMove(gamer.player,40,40);
  	   		break;
  	   		/*
  	   		서울에서 결제
  	   		*/
  	   	case 21: //우주여행 초청장
- 	   		cardMove(gamer,31,31);
+ 	   		cardMove(gamer.player,31,31);
  	   		break;
  	   	case 22: //꽝
  	   		break;
  	   	case 23: //세계일주 초대권
- 	   		plusMoney(gamer,200000);
+ 	   		plusMoney(gamer.player,200000);
  	   		break;
  	   	case 24: //이사(뒤로 두칸)
- 	   		cardMove(gamer,gamer.beforePoint - 2,gamer.beforePoint - 2)
+ 	   		cardMove(gamer.player,gamer.beforePoint - 2,gamer.beforePoint - 2)
  	   		break;
  	   	case 25: //사회복지기금배당
- 	   		cardMove(gamer,39,39)
- 	   		minusMoney(gamer,150000);
+ 	   		cardMove(gamer.player,39,39)
+ 	   		minusMoney(gamer.player,150000);
  	   		break;
  	   		/*
  	   		15만원을 사회복지기금에 배당
@@ -134,14 +134,14 @@ let cardAfterId = 0;
 function minusMoney(player,cardMoney){
 	player.money = player.money - cardMoney;
  	console.log(player.money);
-    $("#player"+player+"money").val(player.money);
+    $("#player"+player.player+"money").val(player.money);
 }
 
 // 황금열쇠 돈 획득
 function plusMoney(player,cardMoney){
 	player.money = player.money + cardMoney;
  	console.log(player.money);
-	$("#player"+player+"money").val(player.money);
+	$("#player"+player.player+"money").val(player.money);
 }
 
 // 황긍열쇠 카드 이동
--- bluemarblegame/WebContent/scripts/playKt.js
+++ bluemarblegame/WebContent/scripts/playKt.js
@@ -1,8 +1,7 @@
 /**
  * 
  */
-$(document).ready(function(){
-	
+
 	//주사위 2개 변수 
 	let firstDice = 0;
 	let secondDice = 0;
@@ -12,6 +11,9 @@ $(document).ready(function(){
 	let move=0;
 	let turn=0;
 	
+	// 게임에 필요한 변수
+	let gamer;	//플레이어
+	let donate; // 기부금
 	
 	function movedice(){
 		firstDice = Math.floor(Math.random()*6+1);//랜덤 출력 1~6
@@ -19,6 +21,18 @@ $(document).ready(function(){
 		gamer=player[turn];
 		console.log("f:",firstDice,"s:",secondDice);
 		gamer.afterPoint = gamer.beforePoint + firstDice + secondDice;
+		
+		//주사위 둘리기 전 위치 태그
+		gamer.beforeId = "#p"+gamer.beforePoint; 
+		
+		//주사위 돌린 후 위치 태그
+		gamer.afterId = "#p"+gamer.afterPoint;
+
+		//주사위 돌리기 전 이미지 삭제
+		$(gamer.beforeId).find(".player"+gamer.player).remove();
+		//캐릭터 출력
+		$(gamer.afterId).append("<input id='pl' class='player"+gamer.player+"' type= 'image' src='/bluemarblegame/img/pin"+gamer.player+".png'>");
+		
 		//한바퀴 돌았을때
 		if(gamer.afterPoint>40){
 			gamer.afterPoint =gamer.afterPoint - 40;
@@ -39,8 +53,61 @@ $(document).ready(function(){
 			gamer.beforePoint = gamer.afterPoint
 			console.log("이동 후 위치:",gamer.beforePoint);
 			
-			if(gamer.afterPoint == 3){
-				goldenKey();
+			//플레이어가 위치한 땅의 이벤트 발생
+			switch(gamer.afterPoint){
+				
+				// 황금열쇠
+				case 3:
+					goldenKey();
+					break;
+				
+				// 황금열쇠
+				case 8:
+					goldenKey();
+					break;	
+					
+				case 11:
+					//무인도
+					break;
+				
+				// 황금열쇠
+				case 13:
+					goldenKey();
+					break;	
+				
+					// 황금열쇠
+				case 18:
+					goldenKey();
+					break;	
+				
+				// 사회복지기금
+				case 21:
+					gamer.money = gamer.money + donate;
+					donate = 0;
+					break;
+				
+					// 황금열쇠
+				case 23:
+					goldenKey();
+					break;	
+					
+				case 31:
+					//우주여행
+					break;
+				
+					// 황금열쇠
+				case 36:
+					goldenKey();
+					break;	
+					
+				case 39:
+					gamer.money = gamer.money-150000;
+					donate += 150000;
+					break;
+				
+				default:
+					break;
+				
 			}
 			
 			//차례를 넘긴다.
@@ -50,12 +117,11 @@ $(document).ready(function(){
 				turn=0;
 			}
 		}
-		console.log("다음 플레이어의 현재위치:",gamer.beforePoint);
+		console.log("플레이어의 현재위치:",gamer.beforePoint);
 		return gamer.beforePoint
 	}
 	//console.log(gamer);
 	$("#dice").click(function(){movedice()});
 
-	
-});
+
 	
